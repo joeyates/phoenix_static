5666deeeecc2006c64b3905f5c7ca74426260a8b
Joe Yates <joe.g.yates@gmail.com>
2025-04-30 10:04:09 +0200

    Introduce the Source behaviour

---
+++ lib/phoenix_static/source.ex
@@ -0,0 +1,28 @@
+defmodule PhoenixStatic.Source do
+  @moduledoc """
+  Defines a behaviour for modules that can be used as a source of static routes.
+  """
+
+  @doc """
+  Returns the content, path and other information for all static pages.
+  """
+  @callback list_pages() :: {:ok, [PhoenixStatic.Page.t()]}
+
+  @doc """
+  Returns the last modified timestamp of the source.
+
+  The return value is a UNIX timestamp.
+
+  Note, the granularity of the timestamp comparisons is one second.
+
+  This function is called by the HTML view to determine
+  whether the source has changed since the last compilation of the module.
+  If the source has changed, the view, along with the related controller and
+  the project's router module, will be recompiled.
+
+  Thsi function should be as fast, reliable and lightweight as possible as it
+  is called on each compilation. Above all, in development mode, it will
+  be called on each request.
+  """
+  @callback last_modified() :: {:ok, integer}
+end
--- test/integration/test_project/config/config.exs
+++ test/integration/test_project/config/config.exs
@@ -1,7 +1,6 @@
 import Config
 
 config :phoenix_static,
-  last_modified: {TestProject.Routes, :last_modified, []},
-  page_source: {TestProject.Routes, :list_pages, []},
+  source: TestProject.Routes,
   controller: TestProjectWeb.GeneratedRoutesController,
   view: TestProjectWeb.GeneratedRoutesHTML
