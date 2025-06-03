5d6cfa72281d28dff094f88509a81a4e5b0a749a
Joe Yates <joe.g.yates@gmail.com>
2025-06-03 13:15:02 +0200

    Use README as main docs page

--- mix.exs
+++ mix.exs
@@ -1,13 +1,16 @@
 defmodule PhoenixStatic.MixProject do
   use Mix.Project
 
+  @version "0.4.1"
+
   def project do
     [
       app: :phoenix_static,
-      version: "0.4.0",
+      version: @version,
       elixir: "~> 1.17",
       deps: deps(),
       description: "Build static pages into a Phoenix application at compile time",
+      docs: docs(),
       elixirc_paths: ["lib"],
       package: package(),
       start_permanent: Mix.env() == :prod
@@ -26,6 +29,21 @@ defmodule PhoenixStatic.MixProject do
       {:phoenix, ">= 0.0.0"},
       {:phoenix_html, "~> 4.1", only: :test},
       {:phoenix_live_view, "~> 1.0.0-rc.1", only: :test}
+    ]
+  end
+
+  defp docs do
+    [
+      source_ref: "v#{@version}",
+      main: "readme",
+      extra_section: "GUIDES",
+      extras: extras()
+    ]
+  end
+
+  defp extras do
+    [
+      "README.md"
     ]
   end
 
