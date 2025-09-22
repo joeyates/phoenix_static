bca8b19dfea99e18d458e6d62093300619c753c1
Joe Yates <joe.g.yates@gmail.com>
2025-09-22 16:36:51 +0200

    Remove debugging

--- test/integration/test_project/config/config.exs
+++ test/integration/test_project/config/config.exs
@@ -1,8 +1,6 @@
 import Config
 
-IO.puts("Loading config.exs")
 phoenix_static_disabled = System.get_env("PHOENIX_STATIC_DISABLED") in ~w(true 1)
-IO.puts("phoenix_static_disabled: #{phoenix_static_disabled}")
 
 config :phoenix_static,
   disabled: phoenix_static_disabled
